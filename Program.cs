2098080fcdccb77e5955cb92063ead55725c350f
Arsen Tevonyan <arsen.tevonyan@us.softvision.global>
2022-10-01 16:18:23 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Puzzle
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {       
+           // disks
+           var n = 3;
+           // source ,  auxil, destin, 
+           towerOfHanoi(n, 'A', 'B', 'C');
+       } 
+        static void towerOfHanoi(int n, char from_Tower,char helper_Tower, char to_Tower )
+        {
+            if (n > 0)
+            {            
+            towerOfHanoi(n - 1, from_Tower,to_Tower, helper_Tower);
+            // showing a step
+            Console.WriteLine("move disk " + n + " from tower " + from_Tower + " to tower " + to_Tower);
+
+            towerOfHanoi(n - 1, helper_Tower, from_Tower, to_Tower);
+            }            
+        }     
+    }
+}
